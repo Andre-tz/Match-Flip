e7288c7a7dd0159d5924b5b6647e9e84438618ae
Andre-tz <italo.tenorio1999@gmail.com>
2025-06-18 21:29:08 -0500

    Created preloadImages helper to preload game images before rendering

--- src/Components/game/Game.tsx
+++ src/Components/game/Game.tsx
@@ -1,10 +1,13 @@
 //este componente tendra todo el juego de memory cards
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import useGame from "../../hook/useGame";
 import CountDown from "./CountDown";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import getRandomImages from "../../herlpers/getRandomImages";
 import Cards from "./Cards";
+import preloadImages from "../../herlpers/preloadImages";
+import { toast } from "sonner";
 
 //este array contiene el nombre de todas las imagenes que se usaran en el juego
 const allImages = [ "blue-eyes", "bombardera", "cortex", "crash", "dark-magician", "deimos", "goku", "kratos", "luigi", "mario", "palito-pez", "red-eyes", "vegeta", "yoshi", "zeus"]
@@ -14,32 +17,56 @@ const Game = () =>{
     const { timerActive, userData } = useGame();
     //obtener el hook para manejar el click en el boton de atras
     const navigation   = useNavigate();
+    //esta constante sirve para condicionar cuando se debe mostrar mi juego
+    const [ loadedImages, setLoadedImages ] = useState<boolean>( false );
+    
     //manejando el click para navegar a otra pagina
     const handleClick = () =>{
         navigation("/") //esto nos lleva al inicio
     }
-    
+
     //aqui usaré el helper que he creado para obtener mis imagenes en una nueva constante
     const gameImages = getRandomImages( allImages,userData.pairCards )
+    
+     //usando mi helper para precargar las imagenes
+    useEffect( ()=>{ 
+        //con esto precargo lo las imagenes unicas no las duplicadas, para evitar guardar en cache iamgenes innecesarias
+        const uniqueGameImage = [ ...new Set( gameImages ) ];
+        preloadImages( uniqueGameImage )
+            .then( ()=>{
+                setLoadedImages( true )
+            })
+            .catch( error =>{
+                console.error( error  )
+                toast.error( "Ocurrió un problema al cargar imágenes" )
+            })
+    }, [ gameImages ])
 
+    useEffect( ()=>{ toast.success( "Imágenes cargadas" ) }, [ loadedImages])
     //cuando el timer esta activo se renderizara el componente CountDown
     if( timerActive ) { return <CountDown /> }
 
-    //cuando el timer llega a 0 enotnces me retornara el juego en si
-    return (
-        <> 
-            <IoMdArrowRoundBack onClick={ handleClick } className="absolute left-4"/>
-            <h1>Componente del juego</h1>
-            <div className="flex flex-wrap">
-                { gameImages.map( ( card, index  ) => (
-                    <Cards
-                        key={ index }
-                        name ={ card }
-                    />
-                ))}
-            </div>
-        </>
-    )
+    if( !loadedImages ){
+        return ( 
+            <h1>Cargando Imagenes</h1>
+        )
+    } else{
+        //cuando el timer llega a 0 enotnces me retornara el juego en si
+        return (
+            <> 
+                <IoMdArrowRoundBack onClick={ handleClick } className="absolute left-4"/>
+                <h1>Componente del juego</h1>
+                <div className="flex flex-wrap">
+                    { gameImages.map( ( card, index  ) => (
+                        <Cards
+                            key={ index }
+                            name ={ card }
+                        />
+                    ))}
+                </div>
+            </>
+        )
+    }
 }
 
 export default Game;
---
+++ src/herlpers/preloadImages.ts
@@ -0,0 +1,19 @@
+// esta archivo sirve para precargar las imagenes antes de iniciar e l juego
+//crea como una copia de la imagen en memoria, por lo que cuando renderices el componente card y vean que las rutas coincidan con la de memoria
+//entonces no neceseitaras vovler a cargar, ya lo tendrá listo
+const preloadImages = ( arrayImages : string[] ) : Promise< void[] > =>{
+    //esta funcion nos va a retornar un lista de promesas 
+    return Promise.all( // con promise all esperara a que se resuelvan las promesas dentro para recien retornar
+        arrayImages.map( path  => {//como promise.all trabaja con lista de promesas. use map para trabajar con cada item de la lista
+            return new Promise<void> ( ( resolve, reject ) =>{ // ahora cada item me devolvera un promesas el cual se puede manejar con resolve  y reject
+                const img = new Image(); // con new Image creamos un etiqueta <img> en memoria 
+                img.src = `/src/images/${ path }.webp`//le damos su srr
+                img.alt = `Imagen de ${ path }`//tambienle damos su texto alternativo
+                img.onload = ()=> resolve();//ccuando la imagen este lista se ejecutara la funcion resolver
+                img.onerror= ()=> reject( "Error al cargar la Imagen" );// si hay un error se ejecutará ña funcion reject
+            })
+        })
+    )
+}
+
+export default preloadImages;// lo exportamos
