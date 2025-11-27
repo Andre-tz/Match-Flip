ae404564f392096b5a51ab147da184f33eb3c850
Andre-tz <italo.tenorio1999@gmail.com>
2025-11-26 21:44:08 -0500

    feat(theme): add font switching logic for normal and code mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,31 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer"
 import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
+import useGame from "./hook/useGame";
+import { useEffect, useState } from "react";
 
 function App() {
+    const { userData } = useGame();
+    //esta constante guardará el tipo de fuente
+    const [ font, setFont ] = useState<string>( "" );
+    //con useEffect cambiara el valor de la variable cada vez que el usuario cambie de tema
+   useEffect( ()=>{
+    if( userData.codeTheme ) { 
+        setFont( "font-['Ubuntu_Mono']")
+    } else { 
+        setFont( "font-['Rubik']" )
+    }
+   }, [ userData.codeTheme])
+
   return (
-    <>
+    <div className={ `${ font } w-[100vw] h-[100vh]`}>
         <Header />
         <main className="bg-white-0 p-4 flex flex-col items-center justify-center min-h-screen relative z-0">
             <Outlet /> 
         </main>
         <Footer />
         <Toaster position="top-center" />
-    </>
+    </div>
   )
 }
 
